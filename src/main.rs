adfb117b18fb80c4db226c24e534689935874875
Jon Gjengset <jon@thesquareplanet.com>
2023-10-19 09:56:05 +0200

    Avoid deep ifs

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use std::env;
 
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
-    if let Some(rest) = encoded_value.strip_prefix('i') {
-        if let Some((digits, _)) = rest.split_once('e') {
-            if let Ok(n) = digits.parse::<i64>() {
-                return n.into();
-            }
-        }
+    if let Some(n) = encoded_value
+        .strip_prefix('i')
+        .and_then(|rest| rest.split_once('e'))
+        .and_then(|(digits, _)| digits.parse::<i64>().ok())
+    {
+        return n.into();
     } else if let Some((len, rest)) = encoded_value.split_once(':') {
         if let Ok(len) = len.parse::<usize>() {
             return serde_json::Value::String(rest[..len].to_string());
