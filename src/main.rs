26e8e0e1821bbdafcfdefe34abcf531aaa60e94a
Jon Gjengset <jon@thesquareplanet.com>
2023-10-19 11:01:59 +0200

    Restore manual impl for old stages of tests

--- src/main.rs
+++ src/main.rs
@@ -84,9 +84,8 @@ fn main() -> anyhow::Result<()> {
 
     match args.command {
         Command::Decode { value } => {
-            // let v: serde_json::Value = serde_bencode::from_str(&value).unwrap();
-            // println!("{v}");
-            unimplemented!("serde_bencode -> serde_json::Value is borked");
+            let v = decode_bencoded_value(&value).0;
+            println!("{v}");
         }
         Command::Info { torrent } => {
             let dot_torrent = std::fs::read(torrent).context("read torrent file")?;
@@ -144,4 +143,61 @@ mod hashes {
             deserializer.deserialize_bytes(HashesVisitor)
         }
     }
+}
+
+// serde_bencode -> serde_json::Value is borked, so keep our manual impl too
+fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
+    match encoded_value.chars().next() {
+        Some('i') => {
+            if let Some((n, rest)) =
+                encoded_value
+                    .split_at(1)
+                    .1
+                    .split_once('e')
+                    .and_then(|(digits, rest)| {
+                        let n = digits.parse::<i64>().ok()?;
+                        Some((n, rest))
+                    })
+            {
+                return (n.into(), rest);
+            }
+        }
+        Some('l') => {
+            let mut values = Vec::new();
+            let mut rest = encoded_value.split_at(1).1;
+            while !rest.is_empty() && !rest.starts_with('e') {
+                let (v, remainder) = decode_bencoded_value(rest);
+                values.push(v);
+                rest = remainder;
+            }
+            return (values.into(), &rest[1..]);
+        }
+        Some('d') => {
+            let mut dict = serde_json::Map::new();
+            let mut rest = encoded_value.split_at(1).1;
+            while !rest.is_empty() && !rest.starts_with('e') {
+                let (k, remainder) = decode_bencoded_value(rest);
+                let k = match k {
+                    serde_json::Value::String(k) => k,
+                    k => {
+                        panic!("dict keys must be strings, not {k:?}");
+                    }
+                };
+                let (v, remainder) = decode_bencoded_value(remainder);
+                dict.insert(k, v);
+                rest = remainder;
+            }
+            return (dict.into(), &rest[1..]);
+        }
+        Some('0'..='9') => {
+            if let Some((len, rest)) = encoded_value.split_once(':') {
+                if let Ok(len) = len.parse::<usize>() {
+                    return (rest[..len].to_string().into(), &rest[len..]);
+                }
+            }
+        }
+        _ => {}
+    }
+
+    panic!("Unhandled encoded value: {}", encoded_value)
 }
